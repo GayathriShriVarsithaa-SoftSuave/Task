5c52b4a8cb2a21d9fa4a8f85a5a6340f129ce21d
GayathriShriVarsithaa-SoftSuave <gayathrishri.varsithaa@softsuave.org>
2026-02-04 13:21:41 +0530

    Implemented basic API

--- app/src/main/java/com/example/task_2/api/RestApiDemoFragment.kt
+++ app/src/main/java/com/example/task_2/api/RestApiDemoFragment.kt
@@ -9,7 +9,6 @@ import com.example.task_2.model.PostRequest
 import com.example.task_2.network.RetrofitInstance
 import kotlinx.coroutines.launch
 
-
 class RestApiDemoFragment : Fragment(R.layout.fragment_rest_api_demo) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
