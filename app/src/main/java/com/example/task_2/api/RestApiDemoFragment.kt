df62fc3b696e9311d9dabbd998eae435438e0ba4
shamy750 <shameeth.suresh@softsuave.org>
2026-02-04 13:16:35 +0530

    Implemented basic API

--- app/src/main/java/com/example/task_2/api/RestApiDemoFragment.kt
+++ app/src/main/java/com/example/task_2/api/RestApiDemoFragment.kt
@@ -4,6 +4,10 @@ import android.widget.Button
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.task_2.R
+import androidx.lifecycle.lifecycleScope
+import com.example.task_2.model.PostRequest
+import com.example.task_2.network.RetrofitInstance
+import kotlinx.coroutines.launch
 
 
 class RestApiDemoFragment : Fragment(R.layout.fragment_rest_api_demo) {
@@ -16,11 +20,44 @@ class RestApiDemoFragment : Fragment(R.layout.fragment_rest_api_demo) {
         val tvResult = view.findViewById<TextView>(R.id.tvResult)
 
         btnGet.setOnClickListener {
-            tvResult.text = "GET API clicked"
+            //tvResult.text = "GET API clicked"
+            lifecycleScope.launch {
+                try {
+                    val response = RetrofitInstance.api.getPost()
+                    if (response.isSuccessful) {
+                        val post = response.body()
+                        tvResult.text = post?.title ?: "No data"
+                    } else {
+                        tvResult.text = "Error: ${response.code()}"
+                    }
+                } catch (e: Exception) {
+                    tvResult.text = "Exception: ${e.localizedMessage}"
+                }
+            }
         }
 
         btnPost.setOnClickListener {
-            tvResult.text = "POST API clicked"
+            //tvResult.text = "POST API clicked"
+            val postRequest = PostRequest(
+                userId = 90,
+                title = "Android POST API",
+                body = "Learning Retrofit with Coroutines"
+            )
+
+            lifecycleScope.launch {
+                try {
+                    val response = RetrofitInstance.api.createPost(postRequest)
+
+                    if (response.isSuccessful) {
+                        tvResult.text = "POST Success:\n${response.body()}"
+                    } else {
+                        tvResult.text = "POST Failed: ${response.code()}"
+                    }
+
+                } catch (e: Exception) {
+                    tvResult.text = "Error: ${e.message}"
+                }
+            }
         }
     }
 }
