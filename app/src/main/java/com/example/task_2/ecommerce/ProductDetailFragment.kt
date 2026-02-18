216de796145f8b3d7e61466250e8c35a38331fea
GayathriShriVarsithaa-SoftSuave <gayathrishri.varsithaa@softsuave.org>
2026-02-18 19:52:58 +0530

    Implemented Base Fragment and BaseViewModel, progress bar loader and shared preference.

--- app/src/main/java/com/example/task_2/ecommerce/ProductDetailFragment.kt
+++ app/src/main/java/com/example/task_2/ecommerce/ProductDetailFragment.kt
@@ -1,72 +1,85 @@
 package com.example.task_2.ecommerce
 
+//import android.content.SharedPreferences
 import android.graphics.Paint
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
+//import androidx.fragment.app.Fragment
+//import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 //import com.example.task_2.R
 //import android.widget.TextView
 import android.widget.Toast
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.bumptech.glide.Glide
-import kotlinx.coroutines.launch
+import com.example.task_2.R
+import com.example.task_2.SessionManager
+import com.example.task_2.base.BaseFragment
+//import com.example.task_2.databinding.FragmentEcommerceBinding
+//import kotlinx.coroutines.launch
 //import android.widget.ImageView
 import com.example.task_2.databinding.FragmentProductDetailBinding
 
-class ProductDetailFragment : Fragment() {
+class ProductDetailFragment :
+    BaseFragment<FragmentProductDetailBinding>(FragmentProductDetailBinding::inflate) {
+    private val viewModel: ProductDetailViewModel by viewModels()
 
-    private var _binding: FragmentProductDetailBinding? = null
-    private val binding get() = _binding!!
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        _binding = FragmentProductDetailBinding.inflate(inflater, container, false)
-        return binding.root
+    override fun setupViews() {
+
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+    override fun observeViewModel() {
+        viewModel.product.observe(viewLifecycleOwner) { product ->
+            binding.textViewTitle.text = product.title
+            binding.textViewDescription.text = product.description
+            binding.logOutButton.setOnClickListener {
+                onClick(it.id)
+            }
+            binding.textViewOriginalPrice.text = "$${product.originalprice}"
+            binding.textViewOriginalPrice.paintFlags =
+                binding.textViewOriginalPrice.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
 
-        val productId = arguments?.getInt("PRODUCT_ID") ?: return
+            binding.textViewDiscountPrice.text = "$${product.discountprice}"
 
-        fetchProductDetails(productId, view)
-    }
+            Glide.with(requireContext())
+                .load(product.image[0])
+                .into(binding.imageViewProduct)
+        }
 
-    private fun fetchProductDetails(id: Int, view: View) {
-        lifecycleScope.launch {
-            try {
+        viewModel.loading.observe(viewLifecycleOwner) { isLoading ->
+            if (isLoading) {
                 binding.progressDetailProgress.visibility = View.VISIBLE
-                val product = RetrofitInstance.api.getProductById(id)
-
-                binding.textViewTitle.text = product.title
-                binding.textViewDescription.text = product.description
-
-                binding.textViewOriginalPrice.text = "$${product.originalprice}"
-                binding.textViewOriginalPrice.paintFlags =
-                    binding.textViewOriginalPrice.paintFlags or Paint.STRIKE_THRU_TEXT_FLAG
-
-                binding.textViewDiscountPrice.text = "$${product.discountprice}"
-
-                Glide.with(requireContext())
-                    .load(product.image[0])
-                    .into(binding.imageViewProduct)
-
-            } catch (e: Exception) {
-                Toast.makeText(requireContext(), "Error ${e.message}", Toast.LENGTH_SHORT).show()
-            } finally {
+            } else {
                 binding.progressDetailProgress.visibility = View.GONE
+            }
+        }
+
+        viewModel.error.observe(viewLifecycleOwner) { message ->
+            Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+        }
+        viewModel.logout.observe(viewLifecycleOwner) {
+            val sessionManager = SessionManager(requireContext())
+            sessionManager.logout()
+            viewModel.logoutend()
+            findNavController().navigate(R.id.logged_out_To_Welcome)
+        }
+    }
+
+    override fun onClick(viewId: Int) {
+        when (viewId) {
+            R.id.logOutButton -> {
+                viewModel.logout()
             }
         }
     }
 
-    override fun onDestroyView() {
-        super.onDestroyView()
-        _binding = null
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val productId = arguments?.getInt("PRODUCT_ID") ?: return
+        viewModel.fetchProduct(productId)
     }
 }
 
